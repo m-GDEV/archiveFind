6eed70cde80d75c25eeb9d761293a906bd9ae9f8
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-01-12 20:53:44 -0500

    removed 12ft only using archive.ph. also built it

--- service_worker.js
+++ service_worker.js
@@ -4,36 +4,15 @@ chrome.action.onClicked.addListener(async (tab) => {
         active: true,
         currentWindow: true,
     });
+
     if (currentTab) {
         const currentTabUrl = currentTab.url;
-
-        // Url to check if 12ft.io will work
-        const apiUrlToCheck = `https://12ft.io/api/proxy?ref=&q${encodeURIComponent(
-            currentTabUrl,
+        const redirectUrl = `https://archive.ph/${encodeURIComponent(
+            currentTabUrl
         )}`;
 
-        let urlToNavigateTo;
-
-        // If 12ft.io has the article go there, if not go to archive.ph
-        // If neccessary this could check an array of different sites to see which one works
-        https: fetch(apiUrlToCheck)
-            .then((res) => res.ok)
-            .then((statusOk) => {
-                if (statusOk) {
-                    return `https://12ft.io/proxy?q=${encodeURIComponent(
-                        currentTabUrl,
-                    )}`;
-                } else {
-                    return `https://archive.ph/${encodeURIComponent(
-                        currentTabUrl,
-                    )}`;
-                    // console.log("Cant find it, redirecting to archive.ph");
-                }
-            })
-            .then((givenUrl) => {
-                // console.log(urlToNavigateTo);
-                // Perform the redirection
-                chrome.tabs.update(currentTab.id, { url: givenUrl });
-            });
+        // Perform the redirection
+        console.log(`Redirecting to ${redirectUrl}...`);
+        chrome.tabs.update(currentTab.id, { url: redirectUrl });
     }
 });
